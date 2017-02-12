f42acb3719079994f21013bf82e8aa61afe49674
bigjackieg <bigjackieg@gmail.com>
2017-02-12 18:31:18 -0500

    updated for ninebord

--- src/Action.java
+++ src/Action.java
@@ -1,8 +1,11 @@
 
+
 public class Action {
 	public int row;
 	public int col;
 	public int util;
+	public int row1;
+	public int col1;
 	
 	public Action(int row, int col, int utility) {
 		this.row = row;
@@ -13,6 +16,12 @@ public class Action {
 	public Action(int row, int col) {
 		this.row = row;
 		this.col = col;
+	}
+	public Action(int row, int col, int row1, int col1){
+		this.row=row;
+		this.col=col;
+		this.row1=row1;
+		this.col1=col1;
 	}
 
 }
